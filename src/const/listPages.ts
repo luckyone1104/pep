78c7e25aaaefc43527bafd80164cd5fe8833c51c
Volodymyr Halchuk <vhalchuk@flexservice.com>
2021-11-26 19:55:59 +0200

    Add default date format const. Refactor listPages routes.

--- src/const/listPages.ts
+++ src/const/listPages.ts
@@ -5,56 +5,56 @@ export const listPages = [
 	{
 		name: ListName.Projects,
 		path: '/projects/list',
-		component: lazy(() => import('src/pages/projects/list')),
+		component: lazy(() => import('src/modules/projects/list')),
 	},
 	{
 		name: ListName.Teams,
 		path: '/teams/list',
-		component: lazy(() => import('src/pages/teams/list')),
+		component: lazy(() => import('src/modules/teams/list')),
 	},
 	{
 		name: ListName.Users,
 		path: '/users/list',
-		component: lazy(() => import('src/pages/users/list')),
+		component: lazy(() => import('src/modules/users/list')),
 	},
 	{
 		name: ListName.Fields,
 		path: '/fields/list',
-		component: lazy(() => import('src/pages/fields/list')),
+		component: lazy(() => import('src/modules/fields/list')),
 	},
 	{
 		name: ListName.DeepLinks,
 		path: '/deeplinks/list',
-		component: lazy(() => import('src/pages/deeplinks/list')),
+		component: lazy(() => import('src/modules/deeplinks/list')),
 	},
 	{
 		name: ListName.Documents,
 		path: '/documents/list',
-		component: lazy(() => import('src/pages/documents/list')),
+		component: lazy(() => import('src/modules/documents/list')),
 	},
 	{
 		name: ListName.Roles,
 		path: '/roles/list',
-		component: lazy(() => import('src/pages/roles/list')),
+		component: lazy(() => import('src/modules/roles/list')),
 	},
 	{
 		name: ListName.FieldGroups,
 		path: '/fieldgroups/list',
-		component: lazy(() => import('src/pages/fieldgroups/list')),
+		component: lazy(() => import('src/modules/fieldgroups/list')),
 	},
 	{
 		name: ListName.Surveys,
 		path: '/surveys/list',
-		component: lazy(() => import('src/pages/surveys/list')),
+		component: lazy(() => import('src/modules/surveys/list')),
 	},
 	{
 		name: ListName.Forms,
 		path: '/forms/list',
-		component: lazy(() => import('src/pages/forms/list')),
+		component: lazy(() => import('src/modules/forms/list')),
 	},
 	{
 		name: ListName.FormTemplates,
 		path: '/formtemplates/list',
-		component: lazy(() => import('src/pages/formtemplates/list')),
+		component: lazy(() => import('src/modules/formtemplates/list')),
 	},
 ];
