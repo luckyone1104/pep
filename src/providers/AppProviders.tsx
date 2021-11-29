2474870e2c882c423d96d36d1c96c450023dafba
luckyone1104 <luckyone1104@gmail.com>
2021-11-29 18:40:05 +0200

    Add LocalizationProvider.

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -5,21 +5,26 @@ import { FilterSidebarProvider } from 'src/components/FilterSidebar';
 import { NotificationsProvider } from './NotificationsProvider';
 import { SnackbarProvider } from 'notistack';
 import { DataQueryProvider } from './DataQueryProvider';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import locale from 'date-fns/locale/ru';
 
 export const AppProviders: FC = ({ children }) => {
 	return (
 		<Router>
-			<SnackbarProvider maxSnack={4}>
-				<NotificationsProvider>
-					<DataQueryProvider>
-						<MenuProvider>
-							<FilterSidebarProvider>
-								{children}
-							</FilterSidebarProvider>
-						</MenuProvider>
-					</DataQueryProvider>
-				</NotificationsProvider>
-			</SnackbarProvider>
+			<LocalizationProvider dateAdapter={AdapterDateFns} locale={locale}>
+				<SnackbarProvider maxSnack={4}>
+					<NotificationsProvider>
+						<DataQueryProvider>
+							<MenuProvider>
+								<FilterSidebarProvider>
+									{children}
+								</FilterSidebarProvider>
+							</MenuProvider>
+						</DataQueryProvider>
+					</NotificationsProvider>
+				</SnackbarProvider>
+			</LocalizationProvider>
 		</Router>
 	);
 };
