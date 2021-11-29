c7eef5a695f24e57dc41cb1b44b9cc65db384d31
luckyone1104 <luckyone1104@gmail.com>
2021-11-29 18:38:54 +0200

    Add ListLayout.

---
+++ src/components/List/ListLayout.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+
+export const ListLayout: FC = ({ children }) => {
+	return (
+		<Box sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			gap: 3,
+		}}>
+			{children}
+		</Box>
+	);
+};
--- src/components/List/index.ts
+++ src/components/List/index.ts
@@ -1,1 +1,2 @@
 export { List } from './List';
+export { ListLayout } from './ListLayout';
