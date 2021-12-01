18ccdd07eea84bdb732edb11eda0c2ad68cb13f0
Volodymyr Halchuk <vhalchuk@flexservice.com>
2021-12-01 19:22:17 +0200

    Add SkeletonFields.

---
+++ src/components/SkeletonFields/SkeletonFields.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { ColumnBalancer } from '../ColumnBalancer';
+import { Skeleton, TextField } from '@mui/material';
+
+type SkeletonFieldsProps = {
+	fieldsCount?: number;
+	maxColumns?: number;
+}
+
+export const SkeletonFields: FC<SkeletonFieldsProps> = (
+	{
+		fieldsCount,
+		maxColumns
+	}
+) => {
+	return (
+		<ColumnBalancer maxColumns={maxColumns}>
+			{Array(fieldsCount).fill(null).map((i, key) => (
+				<Skeleton key={key} sx={{ maxWidth: '100%' }}>
+					<TextField />
+				</Skeleton>
+			))}
+		</ColumnBalancer>
+	);
+};
---
+++ src/components/SkeletonFields/index.ts
@@ -0,0 +1,1 @@
+export { SkeletonFields } from './SkeletonFields';
