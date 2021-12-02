13ca335352d05c0bd41a0bd4bb8fe14579bffe27
Volodymyr Halchuk <vhalchuk@flexservice.com>
2021-12-02 16:00:52 +0200

    Add message prop to ErrorBoundary.

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,7 +1,9 @@
 import Alert from '@mui/material/Alert';
 import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { isUndefined } from '../../utils';
 
 type ErrorBoundaryProps = {
+	message?: string;
 	children: ReactNode;
 }
 
@@ -12,21 +14,24 @@ type ErrorBoundaryState = {
 export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 	public state: ErrorBoundaryState = {
 		hasError: false,
-	}
+	};
 
-	public static getDerivedStateFromError () {
+	public static getDerivedStateFromError() {
 		return { hasError: true };
 	}
 
-	componentDidCatch (error: Error, errorInfo: ErrorInfo) {
+	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
 		console.error(error, errorInfo);
 	}
 
-	render () {
+	render() {
+		const message = this.props.message;
+
 		if (this.state.hasError) {
 			return (
 				<Alert severity="error">
-					Oops.. something went wrong!
+					{message && message}
+					{isUndefined(message) && 'Oops.. something went wrong!'}
 				</Alert>
 			);
 		}
