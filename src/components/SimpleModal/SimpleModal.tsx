2dba9f4b64a1ed48eb0a6a277e33607bfe7ebaab
Volodymyr Halchuk <vhalchuk@flexservice.com>
2022-01-04 18:05:29 +0200

    Add keepMounted prop to SimpleModal (true by default).

--- src/components/SimpleModal/SimpleModal.tsx
+++ src/components/SimpleModal/SimpleModal.tsx
@@ -8,16 +8,23 @@ export type SimpleModalProps = {
 	title?: string;
 	open: boolean;
 	onClose: () => void;
+	keepMounted?: boolean;
 };
 
 export const SimpleModal: FC<SimpleModalProps> = ({
 	title,
 	open,
 	onClose,
+	keepMounted = true /*needs to be mounted by default to prevent mui fields from dancing*/,
 	children,
 }) => {
 	return (
-		<Modal open={open} onClose={onClose} closeAfterTransition>
+		<Modal
+			open={open}
+			onClose={onClose}
+			closeAfterTransition
+			keepMounted={keepMounted}
+		>
 			<Fade in={open}>
 				<Box sx={style}>
 					{title && (
